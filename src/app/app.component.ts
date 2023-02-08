4b013cb4d5648c66506d086832980ba54f78dbd2
Diego Bonura <diego@bonura.dev>
2023-02-08 09:07:31 +0100

    Add kendo grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kendoGridAltRow';
+  public gridData = [
+    {
+      ProductID: 1,
+      ProductName: "Chai",
+      UnitPrice: 18,
+      Category: {
+        CategoryID: 1,
+        CategoryName: "Beverages",
+      },
+    },
+    {
+      ProductID: 2,
+      ProductName: "Chang",
+      UnitPrice: 19,
+      Category: {
+        CategoryID: 1,
+        CategoryName: "Beverages",
+      },
+    },
+    {
+      ProductID: 3,
+      ProductName: "Aniseed Syrup",
+      UnitPrice: 10,
+      Category: {
+        CategoryID: 2,
+        CategoryName: "Condiments",
+      },
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
